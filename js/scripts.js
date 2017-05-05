caf106cc74bf4dfc3edf45af9ee4764c3e6c298b
Jordan Loop <jordloop@MacBook-Air.local>
2017-05-05 13:35:20 -0700

    Add else statement to most recent if statement concatenating <li> + i +</li> to each element passed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,35 @@
-var foo = function(gameLength){
-  var numCheck = isNaN(gameLength);
+//BACK END LOGIC
+var mainFunction = function(numInput){
+  var nanCheck = isNaN(numInput);
   var oneToUserInput = []
-  if (numCheck === true) {
+  if (nanCheck === true) {
     gameLength = "Please enter a number.";
-    return gameLength
+    return numInput
   } else {
-    for (var i = 1; i <=  gameLength; i += 1) {
+    for (var i = 1; i <=  numInput; i += 1) {
       oneToUserInput.push(i);
-    }//for gameLength
+    };//for gameLength
   for (i = 1; i <= oneToUserInput.length; i += 1) { //Adds "Ping", "Pong", "Ping-Pong"
     if (i % 3 === 0) {
-      oneToUserInput.splice(i - 1, 1, "Ping");
+      oneToUserInput.splice(i - 1, 1, "<li> Ping </li>");
     } else if (i % 5 === 0) {
-      oneToUserInput.splice(i - 1, 1, "Pong");
+      oneToUserInput.splice(i - 1, 1, "<li> Pong </li>");
     } else if (i % 10 === 0) {
-      oneToUserInput.splice(i - 1, 1, "Pong-Pong");
-    }//for oneToUserInput
-  }
-    console.log(oneToUserInput);
+      oneToUserInput.splice(i - 1, 1, "<li> Pong-Pong </li>");
+    } else {
+      oneToUserInput.splice(i - 1, 1, "<li>" + i + "</li>");
+    };
+  };
     return oneToUserInput
-  }//main if/else
-}//gameLength function
-
-
-
-
-
+  };//main if/else
+};//mainFunction
 // FRONT END LOGIC
 $(document).ready(function(){
   $("form#userForm").submit(function(event) {
   event.preventDefault();
   var userInput = parseInt($("#input").val());
-  var output = foo(userInput)
-  $(console.log(output));
+  var output = mainFunction(userInput)
   $("#output").empty();
-  $("#output").append("<li>" + output + "<li>");
+  $("#output").append(output);
   });
 });
