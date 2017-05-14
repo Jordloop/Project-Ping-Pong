9b4d191a7e02b58fd3aa8bd9b4bfd128251dcf23
JordLoopgit config --global user.email jordloop@gmail.com <jordloop@MacBook-Air.local>
2017-05-14 10:16:22 -0700

    Add refactored code so that front and back end are seperated, add specs to README.MD

--- README.md
+++ README.md
@@ -22,6 +22,19 @@
 
 ## Specs
 
+* User inputs a number and a list from one to chosen is returned.
+Input: 9   Output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+
+* User inputs a number and a list from one to chosen is returned replacing numbers divisible by 15 as "Ping-Pong".
+Input: 9   Output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, Ping-Pong
+
+* User inputs a number and a list from one to chosen is returned replacing numbers divisible by 5 as "Ping".
+Input: 9   Output: 1, 2, 3, 4, Ping, 6, 7, 8, 9, Ping, 11, 12, 13, 14, Ping-Pong
+
+* User inputs a number and a list from one to chosen is returned replacing numbers divisible by 5 as "Pong".
+Input: 9   Output: 1, 2, Pong, 4, Ping, Pong, 7, 8, 9, Ping, 11, Pong, 13, 14, Ping-Pong
+
+
 ### 1. Receive userInput
 * a. Collect userInput.√
 * b. Parse userInput to num.√
--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,34 @@
 //BACK END LOGIC
 var mainFunction = function(numInput){
-  var nanCheck = isNaN(numInput);
-  var oneToUserInput = []
-  if (nanCheck === true) { // Input that isNaN passes through here.
-    numInput = "Please enter a number.";
-    return numInput
-  } else { // Input that is a number passes through here.
-    for (var i = 1; i <=  numInput; i += 1) {
-      oneToUserInput.push(i);
-    };
-  for (i = 1; i <= oneToUserInput.length; i += 1) { // Checks divisiblility.
+  var oneToUserInput = [];
+
+  for (var i = 1; i <=  numInput; i += 1) { // Builds an array from 1-numInput.
+    oneToUserInput.push(i);
+  };
+
+  var outputArray = oneToUserInput.map(function(i) { // Checks divisiblility.
     if (i % 15 === 0) { // If i is divisible by 15 it passes through here.
-      oneToUserInput.splice(i - 1, 1, "<li> Ping-Pong </li>");
+      return " Ping-Pong"
     } else if (i % 5 === 0) { // If i is divisible by 5 it passes through here.
-      oneToUserInput.splice(i - 1, 1, "<li> Ping </li>");
+      return " Ping";
     } else if (i % 3 === 0) { // If i is divisible by 3 it passes through here.
-      oneToUserInput.splice(i - 1, 1, "<li> Pong </li>");
+      return  " Pong";
     } else { //Everthing else passes through here.
-      oneToUserInput.splice(i - 1, 1, "<li>" + i + "</li>");
+       return " " +i;
     };
-  };
-    return oneToUserInput
-  };
+  });
+    return outputArray
 };
+
 // FRONT END LOGIC
 $(document).ready(function(){
   $("form#userForm").submit(function(event) {
   event.preventDefault();
+
   var userInput = parseInt($("#input").val()); // this var holds the user's input.
   var output = mainFunction(userInput) // This var holds the return of mainFunction.
+
   $("#output").empty(); // Clears out any previous appended data.
-  $("#output").append(output); // Appends the value of output to #output in HTML.
+  $("#output").append("<li>" + " " + output + "</li>"); // Appends the value of output to #output in HTML.
   });
 });
